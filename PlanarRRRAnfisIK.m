71cf9345f766530324302b02eaf5198b2c838eb6
Joshua Cox <48685856+GM18368@users.noreply.github.com>
2019-04-09 01:34:12 +0100

    Add files via upload

--- PlanarRRRAnfisIK.m
+++ PlanarRRRAnfisIK.m
@@ -6,22 +6,28 @@ l1 = 10; % length of first arm
 l2 = 7; % length of second arm
 l3 = 5; % length of third arm
 
-%Data generation
-i=1;
-for theta1 = 0:pi/10:pi
-    
-    for theta2 = 0:pi/20:pi/2
-         
-        for theta3 = -pi/2:pi/10:pi/2
-            THETA1(i) = theta1;
-            THETA2(i) = theta2;
-            THETA3(i) = theta3;
-            i = i+1;
-           
-            
-        end
-    end
-end
+% %Data generation
+% i=1;
+% for theta1 = 0:pi/40:pi
+%     
+%     for theta2 = 0:pi/40:pi/2
+%          
+%         for theta3 = -pi/2:pi/40:pi/2 %used to be from -pi/2
+%             THETA1(i) = theta1;
+%             THETA2(i) = theta2;
+%             THETA3(i) = theta3;
+%             i = i+1;
+%            
+%             
+%         end
+%     end
+% end
+%%
+%alternate generation
+theta1 = 0:0.4:pi; % all possible theta1 values
+theta2 = 0:0.2:pi/2; % all possible theta2 values
+theta3 = -pi/2:0.4:pi/2;
+[THETA1,THETA2,THETA3] = meshgrid(theta1,theta2,theta3);
 
 %%
 %FK calculations
@@ -32,53 +38,124 @@ PHI = THETA1 + THETA2 + THETA3; % Where does this get used?
 %Plot the generated workspace?
 figure(1);
 plot(FKX(:), FKY(:),'.');
-
-data1 = [FKX(:) FKY(:)  PHI(:) THETA1(:)]; % create x-y-phi-theta1 dataset
-data2 = [FKX(:) FKY(:)  PHI(:) THETA2(:)]; % create x-y-phi-theta2 dataset
-data3 = [FKX(:) FKY(:)  PHI(:) THETA3(:)];% create x-y-phi-theta3 dataset
+hold on;
 
 
-opt = anfisOptions;
-opt.InitialFIS = 4;
-opt.EpochNumber = 50;
-opt.DisplayANFISInformation = 0;
-opt.DisplayErrorValues = 0;
-opt.DisplayStepSize = 0;
-opt.DisplayFinalResults = 0;
+
+
+%genfis datasets
+data1 = [FKX(:) FKY(:)  PHI(:) ]; % create x-y-phi-theta1 dataset
+data2 = [FKX(:) FKY(:)  PHI(:) ]; % create x-y-phi-theta2 dataset
+data3 = [FKX(:) FKY(:)  PHI(:) ];% create x-y-phi-theta3 dataset
+%anfis datasets
+fulldata1 = [FKX(:) FKY(:)  PHI(:) THETA1(:) ];
+fulldata2 = [FKX(:) FKY(:)  PHI(:) THETA2(:) ];
+fulldata3 = [FKX(:) FKY(:)  PHI(:) THETA3(:) ];
+
+%%
+%Initial genfis gridpartition
+genOpt = genfisOptions('GridPartition');
+
+%initial genfis subtractive
+%genOpt = genfisOptions('SubtractiveClustering');
+%genOpt.NumClusters = 6;
+
+%Initial genfis clustering?
+ %genOpt = genfisOptions('FCMClustering','FISType','sugeno');
+ %genOpt2 = genfisOptions('FCMClustering','FISType','sugeno');
+ %genOpt3 = genfisOptions('FCMClustering','FISType','sugeno');
+
+%gridpartition options
+ genOpt.NumMembershipFunctions = [3 3 9]; %membership function number for inputs x y and phi
+ genOpt.InputMembershipFunctionType = ["gbellmf" "gbellmf" "gbellmf"];
+ genOpt.OutputMembershipFunctionType =["linear"];
+
+%clustering options
+%genOpt.NumClusters = 8; %Essentially the number of membership functions.
+%genOpt.Verbose = 0; %Suppress output
+% genOpt2.NumClusters = 12; %Essentially the number of membership functions.
+% genOpt2.Verbose = 0; %Suppress output
+% genOpt3.NumClusters = 12; %Essentially the number of membership functions.
+% genOpt3.Verbose = 0; %Suppress output
+
+%set number of epochs for anfis training
+numEpochs = 10;
+
+%genOpt.InputMembershipFunctionType = 'gaussmf';
+disp('--> generating first GENFIS.')
+inFIS1 = genfis(data1,THETA1(:),genOpt);
+disp('--> generating second GENFIS.')
+inFIS2 = genfis(data2,THETA2(:),genOpt);
+disp('--> generating third GENFIS.')
+inFIS3 = genfis(data3,THETA3(:),genOpt);
+
+%%
+%anfis setup
+%opt = anfisOptions;
+%opt.InitialFIS = 6;
+opt1 = anfisOptions('InitialFIS',inFIS1);
+opt1.DisplayANFISInformation = 0;
+opt1.DisplayErrorValues = 0;
+opt1.DisplayStepSize = 0;
+opt1.DisplayFinalResults = 0;
+
+%anfis2
+opt2 = anfisOptions('InitialFIS',inFIS2);
+opt2.DisplayANFISInformation = 0;
+opt2.DisplayErrorValues = 0;
+opt2.DisplayStepSize = 0;
+opt2.DisplayFinalResults = 0;
+%anfis3
+opt3 = anfisOptions('InitialFIS',inFIS3);
+opt3.DisplayANFISInformation = 0;
+opt3.DisplayErrorValues = 0;
+opt3.DisplayStepSize = 0;
+opt3.DisplayFinalResults = 0;
+
+
+
 
 %%
 % Train anfis1.
 disp('--> Training first ANFIS network.')
-anfis1 = anfis(data1,opt);
+opt1.EpochNumber = numEpochs;
+anfis1 = anfis(fulldata1,opt1);
 
 
 %Train anfis2
 disp('--> Training second ANFIS network.')
-opt.InitialFIS = 4;
-opt.EpochNumber = 30;
-anfis2 = anfis(data2,opt);
+% opt.InitialFIS = 5;
+opt2.EpochNumber = numEpochs;
+anfis2 = anfis(fulldata2,opt2);
 
 %train anfis3
 disp('--> Training third ANFIS network.')
-opt.InitialFIS = 4;
-opt.EpochNumber = 10;
-anfis3 = anfis(data3,opt);
+% opt.InitialFIS = 4;
+opt3.EpochNumber = numEpochs;
+anfis3 = anfis(fulldata3,opt3);
+
+%%
+%straight line right side
+ % x coordinates for validation
+% X = 12:-0.1:0; % x coordinates for validation
+% Y(1:121) = 14; % y coordinates for validation
+% phiV(1:121) = 0; % phi for validation
+
+%%
+%straight line left side
+X = -13:0.1:5; % x coordinates for validation
+Y(1:181) = 14 % y coordinates for validation
+phiV(1:181) = pi/2; % phi for validation
 
 
-% X = 10:0.1:15; % x coordinates for validation
-% Y = 15:-0.1:10; % y coordinates for validation, This is just a line currently
-
-x = linspace(0,pi,100);
-X = x + 12;
-Y = sin(x)+12;
 
 
-phiV(1,100) = 0; %phi value for validation
-figure(2);
+% figure(2);
 plot(X,Y,'-b');
+title('Comparison of target and anfis IK)','fontsize',10);
 xlim([-25 25]);
 ylim([0 25]);
-hold on;
+% hold on;
 %Actual IK calculations, for comparison with anfis
 
 
@@ -102,11 +179,11 @@ THETA1D = atan2((a.*k2)-(k1.*b),(a.*k1)+(b.*k2));
 
 %get theta 3
 THETA3D = phiV - (THETA1D + THETA2D);
-
-%%Plot the calculated IK X&Y positions, this is green
+%%
+%Plot the calculated IK X&Y positions, this is green
 valX = (l1 * cos(THETA1D)) + (l2 * cos(THETA1D+ THETA2D)) + (l3 * cos(THETA1D+THETA2D+THETA3D));
 valY = (l1 * sin(THETA1D)) + (l2 * sin(THETA1D+ THETA2D)) + (l3 * sin(THETA1D+THETA2D+THETA3D));
-%plot(valX(:),valY(:),'-g');
+plot(valX(:),valY(:),'-g');
 
 
 XY = [X' Y' phiV'];
@@ -120,32 +197,47 @@ testY = (l1 * sin(THETA1P)) + (l2 * sin(THETA1P+ THETA2P)) + (l3 * sin(THETA1P+T
 plot(testX,testY,'-r');
 hold off;
 
-%error as percentage of the reach radius?
+%%
+%Error as a percentage of reach radius,Calc distance between target and calculated
+error = sqrt((X(:)-testX(:)).^2 + (Y(:)-testY(:)).^2);
+%convert to a percentage of reach radius (22)
 
+percError = error/22 * 100;
+figure(3)
+plot(percError);
+ylabel('Error percentage','fontsize',10);
+xlabel('EE position number');
+title('Error as a percentage of reach radius (22)','fontsize',10);
+
+
+
+
+
+%%
 %mean squared error
 % theta1diff = (THETA1D(:) - THETA1P).^2; 
 % theta2diff = (THETA2D(:) - THETA2P).^2;
 % theta3diff = (THETA3D(:) - THETA3P).^2;
 
-%Original metrics ,Is this a good measure of error?
-theta1diff = THETA1D(:) - THETA1P; 
-theta2diff = THETA2D(:) - THETA2P;
-theta3diff = THETA3D(:) - THETA3P;
+% %Original metrics ,Is this a good measure of error?
+% theta1diff = THETA1D(:) - THETA1P; 
+% theta2diff = THETA2D(:) - THETA2P;
+% theta3diff = THETA3D(:) - THETA3P;
 
 %Plot the errors for each theta
-figure(3);
-subplot(3,1,1);
-plot(theta1diff);
-ylabel('Theta1 Error','fontsize',10)
-title('MSE 1','fontsize',10)
-
-subplot(3,1,2);
-plot(theta2diff);
-ylabel('Theta2 Error','fontsize',10)
-title('MSE 2','fontsize',10)
-
-subplot(3,1,3);
-plot(theta3diff);
-ylabel('Theta3 Error','fontsize',10)
-title('MSE 3','fontsize',10)
+% figure(3);
+% subplot(3,1,1);
+% plot(theta1diff);
+% ylabel('Theta1 Error','fontsize',10)
+% title('MSE 1','fontsize',10)
+% 
+% subplot(3,1,2);
+% plot(theta2diff);
+% ylabel('Theta2 Error','fontsize',10)
+% title('MSE 2','fontsize',10)
+% 
+% subplot(3,1,3);
+% plot(theta3diff);
+% ylabel('Theta3 Error','fontsize',10)
+% title('MSE 3','fontsize',10)
 
--- RRIKMLP.m
+++ RRIKMLP.m
@@ -11,29 +11,32 @@ l1 = 10; % length of first arm
 l2 = 7; % length of second arm
 l3 = 5; % length of third arm
 
-%these arrays need to be the same size, resolution here affects anfis
-%accuracy, these are all increasing with each other so may not be relective
-%of workspace?
-%theta1 = 0:0.025:pi; % all possible theta1 values
-%theta2 = 0:0.0125:pi/2; % all possible theta2 values
-%theta3 = -pi/2:0.025:pi/2; % all possible theta3 values
 
 %Generate workspace data, want a mix of values
-i=1;
-for theta1 = 0:pi/10:pi
-    
-    for theta2 = 0:pi/20:pi/2
-         
-        for theta3 = -pi/2:pi/10:pi/2
-            THETA1(i) = theta1;
-            THETA2(i) = theta2;
-            THETA3(i) = theta3;
-            i = i+1;
-           
-            
-        end
-    end
-end
+% i=1;
+% for theta1 = 0:pi/10:pi
+%     
+%     for theta2 = 0:pi/60:pi/2
+%          
+%         for theta3 = -pi/2:pi/40:pi/2
+%             THETA1(i) = theta1;
+%             THETA2(i) = theta2;
+%             THETA3(i) = theta3;
+%             i = i+1;
+%            
+%             
+%         end
+%     end
+% end
+% 
+
+%%
+%alternate generation, do we need to normalize these?
+theta1 = 0:0.1:pi; % all possible theta1 values
+theta2 = 0:0.2:pi/2; % all possible theta2 values
+theta3 = -pi/2:0.1:pi/2; % all possible theta3 values
+%This gives about 1000 samples
+[THETA1,THETA2,THETA3] = meshgrid(theta1,theta2,theta3);
 
 %%
 %FK calculations
@@ -42,11 +45,15 @@ FKY = (l1 * sin(THETA1)) + (l2 * sin(THETA1 + THETA2)) + (l3 * sin(THETA1 + THET
 
 phi = THETA1 + THETA2 + THETA3; % Where does this get used?
 
-%  figure(1);
-%  plot(FKX,FKY,'.');
-%  title('Generated workspace')
-%  xlabel('x')
-%  ylabel('y')
+ figure(1);
+ plot(FKX(:),FKY(:),'.');
+ title('Generated workspace')
+ grid on;
+ xlabel('x')
+ ylabel('y')
+ xlim([-25 25]);
+ ylim([-10 25]);
+ hold on;
 
 
 %Join data sets and transpose
@@ -57,12 +64,13 @@ Output = [THETA1(:), THETA2(:), THETA3(:)]';
 
 
 %% Network setup & training
-net = feedforwardnet([8 8]); %WAS 13 13
-net.divideParam.trainRatio = 0.6; % training set was 0.7
-net.divideParam.valRatio = 0.2; % validation set 
-net.divideParam.testRatio = 0.2; % test set 
-net.trainParam.goal = 1e-15 ; %Set error goal
-net.trainParam.min_grad = 1e-15/100;
+net = feedforwardnet([13 12],'trainlm'); %first attempt
+%net = feedforwardnet([13 13 13],'trainlm'); %Second attempt
+net.divideParam.trainRatio = 0.6; % training set ratio
+net.divideParam.valRatio = 0.2; % validation set ratio
+net.divideParam.testRatio = 0.2; % test set ratio
+net.trainParam.goal = 1e-6 ; %Set performance error goal
+net.trainParam.min_grad = 1e-6/100; %Set minimum gradient
 % train a neural network
 net.trainParam.epochs = 3000;
 %Train
@@ -71,52 +79,72 @@ net = train(net,Input,Output);
 
 
 %%
-%Some test coordinates, Don't know if we need these?
-X = 10:0.1:15; % x coordinates for validation
-Y = 15:-0.1:10; % y coordinates for validation, This is just a line currently
-PHI(1,51) = 0; % y coordinates for validation, This is just a line currently
+%Some test coordinates, Make sure these are possible! (You'll get an error
+%with validation IK)
+
+%%
+%straight line right side test
+% X = 12:-0.5:0; % x coordinates for validation
+% Y(1:25) = 14 % y coordinates for validation
+% PHI(1:25) = 0; % phi for validation
+
+%%
+%straight line left side test
+% X = -14:0.5:3; % x coordinates for validation
+% Y(1:35) = 14 % y coordinates for validation
+% PHI(1:35) = pi; % phi for validation
+
+%%
+%Curve through workspace test
+% X = 15:-1:-15;
+% Y = 17 - abs(X.^2)/30;
+% PHI = linspace(0.5,2.5,31);
+
+%%
+%curve on right side side
+% Y = 15:-0.5:0;
+% X = 20 - abs(Y.^2)/40;
+% PHI = linspace(1.0,0,31);
+
+%%
+%circle in workspace
+angle = linspace(0,pi,50);
+X = 2*cos(2*angle);
+Y = 15 + 2*sin(2*angle);
+PHI(1:50) = pi/2;
+
+%%
 %Plot test ik
-figure(2);
+% figure(2);
 plot(X(:),Y(:),'-b');
-xlim([-25 25]);
-ylim([0 25]);
-title('Test IK')
-xlabel('x')
-ylabel('y')
+% xlim([-25 25]);
+% ylim([0 25]);
+% title('Comparison of target and calculated IK')
+% xlabel('x')
+% ylabel('y')
 hold on;
 
-
-%Actual IK calculations, for comparison with anfis
-%These are incorrect at the minute
-
-
+%%
+% %Actual IK calculations, for comparison with anfis
+% %These are incorrect at the minute
 a = Y - (l3*sin(PHI)); % Y of wrist
 b = X - (l3*cos(PHI)); % X of wrist
-
 %Get theta2
-
 D = ( ((a.^2)+(b.^2) - l1^2 - l2^2)/(2*l1*l2)); %Costheta2
-
-
 THETA2D = real(acos(D));
-
 %Get theta1
-
 k1 = l2*sin(THETA2D);
 k2 = l1 + l2*cos(THETA2D);
-
-
 THETA1D = atan2((a.*k2)-(k1.*b),(a.*k1)+(b.*k2));
-
 %get theta 3
 THETA3D = PHI - (THETA1D + THETA2D);
 
 
-XY = [X(:) , Y(:), PHI(:)]';
+%%
+XYPHI = [X(:) , Y(:), PHI(:)]';
 %Then we need to compare this to calculated values
-test = net(XY);
-
-%%Plot the analytically calculated IK X&Y 
+test = net(XYPHI);
+%Plot the analytically calculated IK X&Y 
 valX = (l1 * cos(THETA1D(:))) + (l2 * cos(THETA1D(:)+ THETA2D(:))) + (l3 * cos(THETA1D(:)+THETA2D(:)+THETA3D(:)));
 valY = (l1 * sin(THETA1D(:))) + (l2 * sin(THETA1D(:)+ THETA2D(:))) + (l3 * sin(THETA1D(:)+THETA2D(:)+THETA3D(:)));
 plot(valX(:),valY(:),'-g');
@@ -128,31 +156,44 @@ plot(valX(:),valY(:),'-g');
 testX = (l1 * cos(test(1,:))) + (l2 * cos(test(1,:)+ test(2,:))) + (l3 * cos(test(1,:)+test(2,:)+test(3,:)));
 testY = (l1 * sin(test(1,:))) + (l2 * sin(test(1,:)+ test(2,:))) + (l3 * sin(test(1,:)+test(2,:)+test(3,:)));
 %phi = THETA1 + THETA2 + THETA3; % Where does this get used?
- plot(testX(:),testY(:),'-r');
+plot(testX(:),testY(:),'or','MarkerSize',2);
 hold off;
+
+
+%%
+%Error as a percentage of reach radius,Calc distance between target and calculated
+error = sqrt((X(:)-testX(:)).^2 + (Y(:)-testY(:)).^2);
+%convert to a percentage of reach radius (22)
+
+percError = error/22 * 100;
+figure(3)
+plot(percError);
+ylabel('Error percentage','fontsize',10)
+xlabel('Comparison number','fontsize',10)
+title('Error as a percentage of reach radius (22)','fontsize',10)
 
 %%
 %Extract the networks calculated thetas and compare, these aren't the right
 %metrics
-theta1error = THETA1D - test(1,:);
-theta2error = THETA2D - test(2,:);
-theta3error = THETA3D - test(3,:);
+% theta1error = THETA1D - test(1,:);
+% theta2error = THETA2D - test(2,:);
+% theta3error = THETA3D - test(3,:);
 
 
 
-figure(3);
-subplot(3,1,1);
-plot(theta1error);
-ylabel('Theta1 Error','fontsize',10)
-title('MSE 1','fontsize',10)
-
-subplot(3,1,2);
-plot(theta2error);
-ylabel('Theta2 Error','fontsize',10)
-title('MSE 2','fontsize',10)
-
-subplot(3,1,3);
-plot(theta3error);
-ylabel('Theta3 Error','fontsize',10)
-title('MSE 3','fontsize',10)
+% figure(3);
+% subplot(3,1,1);
+% plot(theta1error);
+% ylabel('Theta1 Error','fontsize',10)
+% title('MSE 1','fontsize',10)
+% 
+% subplot(3,1,2);
+% plot(theta2error);
+% ylabel('Theta2 Error','fontsize',10)
+% title('MSE 2','fontsize',10)
+% 
+% subplot(3,1,3);
+% plot(theta3error);
+% ylabel('Theta3 Error','fontsize',10)
+% title('MSE 3','fontsize',10)
 
